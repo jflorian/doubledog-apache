46cddce520596f65d57c89913279c6d693c1c6e4
John Florian <jflorian@doubledog.org>
2010-04-01 19:04:08 -0400

    New - first attempt at https support
    - new certificate and private key for httpd on hector
    - new virtual host for port 443

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,48 +2,78 @@
 
 class apache {
 
+    include openssl
+
     package { "httpd":
-	ensure	=> installed,
+        ensure  => installed,
     }
 
-#   # static file
-#   file { "/CONFIG_PATH/CONFIG_NAME":
-#       # don't forget to verify these!
-#       group	=> "root",
-#       mode    => 640,
-#       owner   => "root",
-#       require => Package["httpd"],
-#       source  => "puppet:///modules/apache/CONFIG_NAME",
-#   }
+    file { "/etc/httpd/conf.d/doubledog.conf":
+        group   => "root",
+        mode    => 640,
+        owner   => "root",
+        require => Package["httpd"],
+        source  => "puppet:///modules/apache/doubledog.conf",
+    }
 
-#   # template file
-#   file { "/CONFIG_PATH/CONFIG_NAME":
-#       content	=> template("apache/CONFIG_NAME"),
-#       # don't forget to verify these!
-#       group	=> "root",
-#       mode    => 640,
-#       owner   => "root",
-#       require => Package["httpd"],
-#   }
+    file { "/etc/httpd/conf.d/doubledog-ssl.conf":
+        group   => "root",
+        mode    => 640,
+        owner   => "root",
+        require => Package["httpd"],
+        source  => "puppet:///modules/apache/doubledog-ssl.conf",
+    }
 
-    exec { "open-httpd-ports":
-        command => "lokkit --port=80:tcp --port=443:tcp",
-        unless  => "grep -q -- '-A INPUT .* -p tcp --dport 80 -j ACCEPT' /etc/sysconfig/iptables &&
-                    grep -q -- '-A INPUT .* -p tcp --dport 443 -j ACCEPT' /etc/sysconfig/iptables",
+    file { "/etc/pki/tls/private/doubledog-httpd.key":
+        group   => "root",
+        mode    => 600,
+        owner   => "root",
+        require => Package["openssl"],
+        source  => "puppet:///modules/apache/doubledog-httpd.key",
+    }
+
+    file { "/etc/pki/tls/certs/doubledog-httpd.crt":
+        group   => "root",
+        mode    => 600,
+        owner   => "root",
+        require => Package["openssl"],
+        source  => "puppet:///modules/apache/doubledog-httpd.crt",
+    }
+
+    # Web content is reached via NFS, so selinux must be adjusted to allow the
+    # apache daemon to access it.
+    exec { "allow-httpd-use-nfs":
+        command => "setsebool -P httpd_use_nfs on",
+        unless  => "getsebool httpd_use_nfs | grep -q -- 'on$'",
+    }
+
+    exec { "open-http-port":
+        command => "lokkit --port=80:tcp",
+        unless  => "grep -q -- '-A INPUT .* -p tcp --dport 80 -j ACCEPT' /etc/sysconfig/iptables",
+    }
+
+    exec { "open-https-port":
+        command => "lokkit --port=443:tcp",
+        unless  => "grep -q -- '-A INPUT .* -p tcp --dport 443 -j ACCEPT' /etc/sysconfig/iptables",
     }
 
     service { "httpd":
-        enable		=> true,
-        ensure		=> running,
-        hasrestart	=> true,
-        hasstatus	=> true,
-        require		=> [
-            Exec["open-httpd-ports"],
+        enable          => true,
+        ensure          => running,
+        hasrestart      => true,
+        hasstatus       => true,
+        require         => [
+            Exec["allow-httpd-use-nfs"],
+            Exec["open-http-port"],
+            Exec["open-https-port"],
             Package["httpd"],
         ],
-#       subscribe	=> [
-#           File["/CONFIG_PATH/CONFIG_NAME"],
-#       ],
+        subscribe       => [
+            File["/etc/httpd/conf.d/doubledog.conf"],
+            File["/etc/httpd/conf.d/doubledog-ssl.conf"],
+            File["/etc/pki/tls/private/doubledog-httpd.key"],
+            File["/etc/pki/tls/certs/doubledog-httpd.crt"],
+        ],
     }
 
 }
