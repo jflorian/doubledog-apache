8297342b0c07b5181b72ec28c1ae9696807f297e
John Florian <jflorian@doubledog.org>
2010-06-08 19:46:48 -0400

    New - apache::mod_python class

---
+++ manifests/classes/mod_python.pp
@@ -0,0 +1,16 @@
+# /etc/puppet/modules/apache/manifests/classes/mod_python.pp
+
+# Synopsis:
+#       Include this class in addition to the apache class, if mod_python is needed.
+#
+# Example:
+#       include apache
+#       include apache::mod_python
+
+class apache::mod_python {
+
+    package { "mod_python":
+        ensure  => installed,
+    }
+
+}
