b7a11642cdf048e577e802c12a578c31d6e78388
John Florian <jflorian@doubledog.org>
2010-06-27 21:55:06 -0400

    Fix - not using puppet's native selboolean type
    Had been mimicing this with an exec, but this is much cleaner.

--- manifests/classes/apache.pp
+++ manifests/classes/apache.pp
@@ -17,9 +17,9 @@ class apache {
     }
 
     # Web content is reached via NFS, so selinux must be adjusted to allow the apache daemon to access it.
-    exec { "allow-httpd-use-nfs":
-        command => "setsebool -P httpd_use_nfs on",
-        unless  => "getsebool httpd_use_nfs | grep -q -- 'on$'",
+    selboolean { "httpd_use_nfs":
+        persistent      => true,
+        value           => on,
     }
 
     exec { "open-http-port":
@@ -33,9 +33,9 @@ class apache {
         hasrestart      => true,
         hasstatus       => true,
         require         => [
-            Exec["allow-httpd-use-nfs"],
             Exec["open-http-port"],
             Package["httpd"],
+            Selboolean["httpd_use_nfs"],
         ],
         subscribe       => [
             File["/etc/httpd/conf/httpd.conf"],
