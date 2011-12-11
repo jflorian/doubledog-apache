ceae46ffa62b9a42429dd1f95089c57c59168b0c
John Florian <jflorian@doubledog.org>
2011-12-11 17:05:25 -0500

    New - apache support for Fedora 16

--- manifests/classes/apache.pp
+++ manifests/classes/apache.pp
@@ -5,40 +5,43 @@ class apache {
     include lokkit
     include openssl
 
-    package { "httpd":
+    package { 'httpd':
         ensure  => installed,
     }
 
-    file { "/etc/httpd/conf/httpd.conf":
+    file { '/etc/httpd/conf/httpd.conf':
+        # New templates are best made from pristine copies from the target OS
+        # with local changes applied atop that.
         content	=> template("apache/httpd.conf.${operatingsystem}.${operatingsystemrelease}"),
-        group   => "root",
-        mode    => "0640",
-        owner   => "root",
-        require => Package["httpd"],
+        group   => 'root',
+        mode    => '0640',
+        owner   => 'root',
+        require => Package['httpd'],
     }
 
-    # Web content is reached via NFS, so selinux must be adjusted to allow the apache daemon to access it.
-    selboolean { "httpd_use_nfs":
+    # Web content is reached via NFS, so selinux must be adjusted to allow the
+    # apache daemon to access it.
+    selboolean { 'httpd_use_nfs':
         persistent      => true,
         value           => on,
     }
 
-    lokkit::tcp_port { "http":
-        port    => "80",
+    lokkit::tcp_port { 'http':
+        port    => '80',
     }
 
-    service { "httpd":
+    service { 'httpd':
         enable          => true,
         ensure          => running,
         hasrestart      => true,
         hasstatus       => true,
         require         => [
-            Exec["open-http-tcp-port"],
-            Package["httpd"],
-            Selboolean["httpd_use_nfs"],
+            Exec['open-http-tcp-port'],
+            Package['httpd'],
+            Selboolean['httpd_use_nfs'],
         ],
         subscribe       => [
-            File["/etc/httpd/conf/httpd.conf"],
+            File['/etc/httpd/conf/httpd.conf'],
         ],
     }
 
